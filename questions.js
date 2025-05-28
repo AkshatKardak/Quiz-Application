41575faefaf4553d38facc39514441d41519b5b1
AkshatKardak Github <kardakakshat@gmail.com>
2025-05-28 23:38:27 +0530

    Inital commit of quiz application files

---
+++ questions.js
@@ -0,0 +1,49 @@
+const questions =[
+{
+    question: "What is the full form of html?",
+    answers: [
+        {text:"HyperLinks and text Markup Language", correct:false},
+        {text:"Hyper Tool Markup Language", correct:false},
+        {text:"HyperText Markup Language", correct:true},
+        {text:"HypoText Mark Language", correct:false}
+    ]
+},
+ {
+    question: "Which tag is used to create a hyperlink?",
+    answers:[
+        {text:"<link>",correct:false},
+        {text:"<ref>",correct:false},
+        {text:"<url>",correct:false},
+        {text:"<a>",correct:true}
+         
+    ]
+ },
+ {
+    question:"Which property is used to change text color?",
+    answers:[
+        {text:"text-color",correct:false},
+        {text:"color",correct:true},
+        {text:"font-color",correct:false},
+        {text:"text-style",correct:false}
+    ]
+ },
+{
+    question:"How can you display a message in the browser console?",
+    answers:[
+        {text:'alert("Hello")',correct:false},
+        {text:'console.log("Hello")',correct:true},
+        {text:'print("Hello")',correct:false},
+        {text:'show("Hello")',correct:false}
+    ]
+},
+{
+    question:"What does JavaScript do in a website?",
+    answers:[
+    {text:"Adds interactivity",correct:true},
+    {text:"Adds style",correct:false},
+    {text:"Define page structure",correct:false},
+    {text:"Stores data",correct:false}
+    ]
+},
+
+];
